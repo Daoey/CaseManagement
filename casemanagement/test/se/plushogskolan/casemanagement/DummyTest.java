96fdede127d8c42e045529253d407e3e897ee312
Johan Åsling <johan.asling@hotmail.com>
2016-09-27 17:30:57 +0200

    Created case management project

---
+++ casemanagement/test/se/plushogskolan/casemanagement/DummyTest.java
@@ -0,0 +1,14 @@
+package se.plushogskolan.casemanagement;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
