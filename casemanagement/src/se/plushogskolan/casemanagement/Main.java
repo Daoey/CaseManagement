70933289b676859f64afec3aa0a626c3a02f589d
Joakim Landström <joakim.landstrom@outlook.com>
2016-09-28 13:25:33 +0200

    test

--- casemanagement/src/se/plushogskolan/casemanagement/Main.java
+++ casemanagement/src/se/plushogskolan/casemanagement/Main.java
@@ -4,7 +4,7 @@ public final class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Johan!");
-        System.out.println("Hello Johan!");
+        System.out.println("Hello wat!");
     }
 
 }
