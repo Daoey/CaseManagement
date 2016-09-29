a1161ea015d25425109c89c65aef3ae4215936eb
Johan Åsling <johan.asling@hotmail.com>
2016-09-29 11:14:18 +0200

    Added work item status enum and cleaned repository to match model

--- casemanagement/src/se/plushogskolan/casemanagement/model/WorkItem.java
+++ casemanagement/src/se/plushogskolan/casemanagement/model/WorkItem.java
@@ -5,20 +5,26 @@ public final class WorkItem {
     private final int id;
     private final int userId;
     private final String description;
+    private final Status status;
+    
+    public enum Status {
+        UNSTARTED, STARTED, DONE
+    }
 
-    private WorkItem(int id, int userId, String description) {
+    private WorkItem(int id, int userId, String description, Status status) {
         this.id = id;
         this.userId = userId;
         this.description = description;
+        this.status = status;
     }
 
-    public static WorkItemBuilder builder(int userId, String description) {
-        return new WorkItemBuilder(userId, description);
+    public static WorkItemBuilder builder(int userId, String description, Status status) {
+        return new WorkItemBuilder(userId, description, status);
     }
 
     @Override
     public String toString() {
-        return "[id = " + id + ", userId = " + userId + ", description = " + description + "]";
+        return "[id = " + id + ", userId = " + userId + ", description = " + description + ", Status = " + status + "]";
     }
 
     @Override
@@ -29,7 +35,7 @@ public final class WorkItem {
         if (obj instanceof WorkItem) {
             WorkItem otherWorkItem = (WorkItem) obj;
             return id == otherWorkItem.id && userId == otherWorkItem.userId
-                    && description.equals(otherWorkItem.description);
+                    && description.equals(otherWorkItem.description) && status.equals(otherWorkItem.status);
         }
         return false;
     }
@@ -54,15 +60,21 @@ public final class WorkItem {
     public String getDescription() {
         return description;
     }
+    
+    public Status getStatus() {
+        return status;
+    }
 
     public static class WorkItemBuilder {
         private int id = 0;
         private int userId;
         private String description;
+        private Status status;
 
-        private WorkItemBuilder(int userId, String description) {
+        private WorkItemBuilder(int userId, String description, Status status) {
             this.userId = userId;
             this.description = description;
+            this.status = status;
         }
 
         public WorkItemBuilder setId(int id) {
@@ -71,7 +83,7 @@ public final class WorkItem {
         }
 
         public WorkItem build() {
-            return new WorkItem(id, userId, description);
+            return new WorkItem(id, userId, description, status);
         }
 
     }
