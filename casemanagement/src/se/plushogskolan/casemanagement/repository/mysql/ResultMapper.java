83c283c89133b7aae4cf6e0e2c1ce01ebe11bbde
Per-Erik <pe.ferb@gmail.com>
2016-09-29 14:11:55 +0200

    ResultMapper only throws SqlExceptions

--- casemanagement/src/se/plushogskolan/casemanagement/repository/mysql/ResultMapper.java
+++ casemanagement/src/se/plushogskolan/casemanagement/repository/mysql/ResultMapper.java
@@ -3,8 +3,6 @@ package se.plushogskolan.casemanagement.repository.mysql;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import se.plushogskolan.casemanagement.exception.RepositoryException;
-
 public interface ResultMapper<T> {
-    T map(ResultSet resultSet) throws SQLException, RepositoryException;
+    T map(ResultSet resultSet) throws SQLException;
 }
