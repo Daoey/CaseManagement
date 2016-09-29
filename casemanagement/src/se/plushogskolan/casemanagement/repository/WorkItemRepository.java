bfdee798ea28f3bef33d02fd36b7463963e3bd4e
leo MBP <leo.moralesvega@gmail.com>
2016-09-29 18:48:23 +0200

    Added throws RepositoryException to "saveWorkItem" in WorkItemRepository

--- casemanagement/src/se/plushogskolan/casemanagement/repository/WorkItemRepository.java
+++ casemanagement/src/se/plushogskolan/casemanagement/repository/WorkItemRepository.java
@@ -2,11 +2,12 @@ package se.plushogskolan.casemanagement.repository;
 
 import java.util.List;
 
+import se.plushogskolan.casemanagement.exception.RepositoryException;
 import se.plushogskolan.casemanagement.model.WorkItem;
 
 public interface WorkItemRepository {
 
-    void saveWorkItem(WorkItemRepository workItem);
+    void saveWorkItem(WorkItem workItem) throws RepositoryException;
     
     void updateStatusById(int workItemId, WorkItem.Status workItemStatus);
     
