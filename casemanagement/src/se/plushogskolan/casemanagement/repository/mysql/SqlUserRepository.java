a391c0c81cc4d3b61e3bdb583a888f7e92926c1b
Joakim Landström <joakim.landstrom@outlook.com>
2016-09-29 15:34:51 +0200

    mod sqluserrepo

--- casemanagement/src/se/plushogskolan/casemanagement/repository/mysql/SqlUserRepository.java
+++ casemanagement/src/se/plushogskolan/casemanagement/repository/mysql/SqlUserRepository.java
@@ -6,19 +6,20 @@ import java.util.List;
 import se.plushogskolan.casemanagement.model.User;
 import se.plushogskolan.casemanagement.repository.UserRepository;
 
-public class SqlUserRepository implements UserRepository{
+public class SqlUserRepository implements UserRepository {
 
     private final String url = "jdbc:mysql://localhost:3306/case_db?user=root&password=root&useSSL=false";
 
     @Override
     public void saveUser(User user) {
-        
-        try{
-            new SqlHelper(url).query("insert into user_table"
-                    + " set first_name=?, last_name=?, username=?, active=?, "
-                    + "idteam = (select idteam_table from team_table where name like ?)")
-            .parameter(user.getFirstName()).parameter(user.getLastName()).parameter(user.getUsername()).parameter(user.isActive()).
-        }catch (SQLException e) {
+
+        try {
+            new SqlHelper(url)
+                    .query("insert into user_table" + " set first_name=?, last_name=?, username=?, active=?, "
+                            + "idteam = (select idteam_table from team_table where idteam_table like ?)")
+                    .parameter(user.getFirstName()).parameter(user.getLastName()).parameter(user.getUsername())
+                    .parameter(user.isActive()).parameter(user.getTeamId()).update();
+        } catch (SQLException e) {
             // TODO: handle exception
         }
     }
@@ -26,13 +27,13 @@ public class SqlUserRepository implements UserRepository{
     @Override
     public void updateUser(User newValues) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void inactivateUserById(int userId) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -52,7 +53,5 @@ public class SqlUserRepository implements UserRepository{
         // TODO Auto-generated method stub
         return null;
     }
-    
-    
-    
+
 }
