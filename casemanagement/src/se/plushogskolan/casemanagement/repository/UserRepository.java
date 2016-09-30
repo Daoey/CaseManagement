32c18e46c033e6fcdd34ef17710de00c49532c24
Joakim Landström <joakim.landstrom@outlook.com>
2016-09-30 12:30:13 +0200

    save and update user

--- casemanagement/src/se/plushogskolan/casemanagement/repository/UserRepository.java
+++ casemanagement/src/se/plushogskolan/casemanagement/repository/UserRepository.java
@@ -2,20 +2,21 @@ package se.plushogskolan.casemanagement.repository;
 
 import java.util.List;
 
+import se.plushogskolan.casemanagement.exception.RepositoryException;
 import se.plushogskolan.casemanagement.model.User;
 
 public interface UserRepository {
 
-    void saveUser(User user);
+    void saveUser(User user) throws RepositoryException;
 
-    void updateUser(User newValues);
+    void updateUser(User newValues) throws RepositoryException;
     
-    void inactivateUserById(int userId);
+    void inactivateUserById(int userId) throws RepositoryException;
     
-    User getUserById(int userId);
+    User getUserById(int userId) throws RepositoryException;
     
-    User getUserBy(String firstName, String lastName, String username);
+    User getUserBy(String firstName, String lastName, String username) throws RepositoryException;
     
-    List<User> getUsersByTeamId(int teamId);
+    List<User> getUsersByTeamId(int teamId) throws RepositoryException;
     
 }
--- casemanagement/src/se/plushogskolan/casemanagement/repository/mysql/SqlUserRepository.java
+++ casemanagement/src/se/plushogskolan/casemanagement/repository/mysql/SqlUserRepository.java
@@ -3,6 +3,7 @@ package se.plushogskolan.casemanagement.repository.mysql;
 import java.sql.SQLException;
 import java.util.List;
 
+import se.plushogskolan.casemanagement.exception.RepositoryException;
 import se.plushogskolan.casemanagement.model.User;
 import se.plushogskolan.casemanagement.repository.UserRepository;
 
@@ -11,29 +12,42 @@ public class SqlUserRepository implements UserRepository {
     private final String url = "jdbc:mysql://localhost:3306/case_db?user=root&password=root&useSSL=false";
 
     @Override
-    public void saveUser(User user) {
+    public void saveUser(User user) throws RepositoryException {
+
+        String query = "insert into user_table set first_name=?, last_name=?, username=?, active=?, "
+                + "idteam = (select idteam_table from team_table where idteam_table = ?)";
 
         try {
-            new SqlHelper(url)
-                    .query("insert into user_table" + " set first_name=?, last_name=?, username=?, active=?, "
-                            + "idteam = (select idteam_table from team_table where idteam_table like ?)")
-                    .parameter(user.getFirstName()).parameter(user.getLastName()).parameter(user.getUsername())
-                    .parameter(user.isActive()).parameter(user.getTeamId()).update();
+            new SqlHelper(url).query(query).parameter(user.getFirstName()).parameter(user.getLastName())
+                    .parameter(user.getUsername()).parameter(user.isActive()).parameter(user.getTeamId()).update();
         } catch (SQLException e) {
-            // TODO: handle exception
+            throw new RepositoryException("Coulnt insert user with id: " + user.getId(), e);
         }
     }
 
     @Override
-    public void updateUser(User newValues) {
-        // TODO Auto-generated method stub
+    public void updateUser(User newValues) throws RepositoryException {
+
+        String query = "update user_table set first_name=?, last_name=?, username=?, active=?, "
+                + "idteam = (select idteam_table from team_table where idteam_table = ?)"
+                + "where iduser_table = ?";
+
+        try {
+            new SqlHelper(url).query(query).parameter(newValues.getFirstName()).parameter(newValues.getLastName())
+                    .parameter(newValues.getUsername()).parameter(newValues.isActive()).parameter(newValues.getTeamId())
+                    .parameter(newValues.getId()).update();
+
+        } catch (SQLException e) {
+            throw new RepositoryException("Couldnt update user with id: " + newValues.getId(), e);
+        }
 
     }
 
     @Override
     public void inactivateUserById(int userId) {
-        // TODO Auto-generated method stub
 
+        String query = "";
+        
     }
 
     @Override
