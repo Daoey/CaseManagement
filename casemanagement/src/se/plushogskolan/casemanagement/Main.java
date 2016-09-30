5da366523eea6d2b5c9426461d6f086ea782f9da
Per-Erik <pe.ferb@gmail.com>
2016-09-30 13:38:38 +0200

    Presentation off inactivateUser and getUserById

--- casemanagement/src/se/plushogskolan/casemanagement/Main.java
+++ casemanagement/src/se/plushogskolan/casemanagement/Main.java
@@ -9,29 +9,38 @@ import se.plushogskolan.casemanagement.service.CaseService;
 
 public final class Main {
     private static final CaseService CASE_SERVICE = createSqlCaseService();
+    private static final int ID = 0;
+    private static final String USERNAME = "Billy_the_Tester";
+    private static final String FIRST_NAME = "Test";
+    private static final String lAST_NAME = "Testsson";
+    private static final String NEW_USERNAME = "Updated_Tester";
 
     public static void main(String[] args) {
-        int defaultId = 0;
-        String defaultUsername = "Billy_the_Tester";
-        String defaultFirstName = "Test";
-        String defaultLastName = "Testsson";
-        String newUserName = "Updated_Tester";
 
         // USER CRITERIAS
         // Create User
-        User user = createUser(defaultId, defaultUsername, defaultFirstName, defaultLastName);
+        User user = createUser(ID, USERNAME, FIRST_NAME, lAST_NAME);
 
         // Update User
-        updateUser(user, newUserName);
-        updateUser(user, defaultUsername); // reset update
+        updateUser(user, NEW_USERNAME);
+        updateUser(user, USERNAME); // reset update
 
         // Inactivate User
         inactivateUser();
+        
+        // Get User by id
+        getUserById();
+    }
+
+    private static void getUserById() {
+        User userToGet = CASE_SERVICE.getUserBy(FIRST_NAME, lAST_NAME, USERNAME);
+        int id = userToGet.getId();
+        User userById = CASE_SERVICE.getUserById(id);
     }
 
     private static void inactivateUser() {
-        // TODO Auto-generated method stub
-
+        User userToInactivate = CASE_SERVICE.getUserBy(FIRST_NAME, lAST_NAME, USERNAME);
+        CASE_SERVICE.inactivateUserById(userToInactivate.getId());
     }
 
     private static User createUser(int id, String username, String firstName, String lastName) {
