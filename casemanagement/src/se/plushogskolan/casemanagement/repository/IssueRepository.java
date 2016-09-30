37fefe235f5e7febc13423c52c81eb4906e948da
Johan Åsling <johan.asling@hotmail.com>
2016-09-30 14:16:34 +0200

    Added throws Repository exception to Issue Repository

--- casemanagement/src/se/plushogskolan/casemanagement/repository/IssueRepository.java
+++ casemanagement/src/se/plushogskolan/casemanagement/repository/IssueRepository.java
@@ -1,11 +1,12 @@
 package se.plushogskolan.casemanagement.repository;
 
+import se.plushogskolan.casemanagement.exception.RepositoryException;
 import se.plushogskolan.casemanagement.model.Issue;
 
 public interface IssueRepository {
 
-    void saveIssue(Issue issue);
+    void saveIssue(Issue issue) throws RepositoryException;
     
-    void updateIssue(Issue newValues);
+    void updateIssue(Issue newValues) throws RepositoryException;
     
 }
