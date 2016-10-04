23117c2578f0dd69043542de1458cae19ece33cf
leo MBP <leo.moralesvega@gmail.com>
2016-10-04 22:52:30 +0200

    Removed workItemIsDone2 from CaseService

--- casemanagement/src/se/plushogskolan/casemanagement/service/CaseService.java
+++ casemanagement/src/se/plushogskolan/casemanagement/service/CaseService.java
@@ -398,19 +398,6 @@ public final class CaseService {
         }
     }
 
-    // TODO should replace workItemIsDone
-    private boolean workItemIsDone2(int workItemId) {
-
-        WorkItem workItem;
-        try {
-            workItem = workItemRepository.getWorkItemById(workItemId);
-            return WorkItem.Status.DONE.equals(workItem.getStatus());
-        } catch (RepositoryException e) {
-            throw new ServiceException("Could not get WorkItem with id " + workItemId, e);
-
-        }
-    }
-
     private void cleanRelatedDataOnWorkItemDelete(int workItemId) {
         // TODO Implement me
         try {
