37b7ff900ca0f85ee302fac47060dd5ff2197480
Per-Erik <pe.ferb@gmail.com>
2016-10-05 09:42:06 +0200

    saveTeam() "fully" tested

--- casemanagement/test/se/plushogskolan/casemanagement/repository/sql/TestSqlTeamRepository.java
+++ casemanagement/test/se/plushogskolan/casemanagement/repository/sql/TestSqlTeamRepository.java
@@ -1,5 +1,9 @@
 package se.plushogskolan.casemanagement.repository.sql;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +14,7 @@ import se.plushogskolan.casemanagement.repository.mysql.SqlTeamRepository;
 
 public final class TestSqlTeamRepository {
     private Team testTeam;
+    private List<Team> teams;
     private SqlTeamRepository sqlTeamRepository;
 
     @Before
@@ -19,13 +24,22 @@ public final class TestSqlTeamRepository {
     }
 
     @After
-    public void cleanDatabase() throws RepositoryException {
+    public void clean() throws RepositoryException {
         sqlTeamRepository.deleteFromDatabaseTeamWithNameAndActiveStatus(testTeam.getName(), testTeam.isActive());
+        teams = null;
     }
 
     @Test
     public void saveTeam() throws RepositoryException {
         sqlTeamRepository.saveTeam(testTeam);
+
+        teams = sqlTeamRepository.getAllTeams();
+
+        boolean ourTeamFoundInDatabase = false;
+        for (Team team : teams) {
+            if (testTeam.getName().equals(team.getName())) ourTeamFoundInDatabase = true;
+        }
+        assertEquals(true, ourTeamFoundInDatabase);
     }
 
     @Test
