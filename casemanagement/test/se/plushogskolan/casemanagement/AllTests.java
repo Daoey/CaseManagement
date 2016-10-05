0e0e7b59aaadb0a4d811e06dc9e4c9ad8e3194a6
Per-Erik <pe.ferb@gmail.com>
2016-10-05 15:06:02 +0200

    AllTests up to date

--- casemanagement/test/se/plushogskolan/casemanagement/AllTests.java
+++ casemanagement/test/se/plushogskolan/casemanagement/AllTests.java
@@ -7,10 +7,13 @@ import se.plushogskolan.casemanagement.model.TestIssue;
 import se.plushogskolan.casemanagement.model.TestTeam;
 import se.plushogskolan.casemanagement.model.TestUser;
 import se.plushogskolan.casemanagement.model.TestWorkItem;
+import se.plushogskolan.casemanagement.repository.sql.TestSqlIssueRepository;
+import se.plushogskolan.casemanagement.repository.sql.TestSqlTeamRepository;
 
 @RunWith(Suite.class)
 
-@Suite.SuiteClasses({ TestIssue.class, TestTeam.class, TestUser.class, TestWorkItem.class })
+@Suite.SuiteClasses({ TestIssue.class, TestTeam.class, TestUser.class, TestWorkItem.class, TestSqlTeamRepository.class,
+        TestSqlIssueRepository.class })
 
 public class AllTests {
 }
