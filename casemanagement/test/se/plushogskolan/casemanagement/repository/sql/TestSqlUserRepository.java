48a5515ccaab829f3a346520453ca97d1767b999
Joakim Landström <joakim.landstrom@outlook.com>
2016-10-06 14:53:44 +0200

    class for testing user

---
+++ casemanagement/test/se/plushogskolan/casemanagement/repository/sql/TestSqlUserRepository.java
@@ -0,0 +1,14 @@
+package se.plushogskolan.casemanagement.repository.sql;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSqlUserRepository {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
